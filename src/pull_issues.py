b9923750e62eaa13bac8314856ec96120cf29d49
Karma Riuk <riukkarma@gmail.com>
2024-09-23 16:33:32 +0200

    cleaned up unnecessary arguments and print statements

--- src/pull_issues.py
+++ src/pull_issues.py
@@ -12,8 +12,6 @@ def pull_issues(
         github_repo: str, 
         auth_token: str, 
         force_pull=False, 
-        start_id: int = 0, 
-        end_id:int = -1
     ) -> pd.DataFrame:
     """
     Get the issues from the given url.
@@ -22,8 +20,6 @@ def pull_issues(
         github_repo (str): the url of the github repository
         auth_token (str): the authentication token
         force_pull (bool, optional, default = False): force to pull the data. If False, the value returned is the one cached locally (if any), if True pulls and caches a new version
-        start_page (int, optional, default = 0): the page from which we start to fetch issues
-        end_page (int, optional, default = -1): the final page to fetch. -1 means the last page
 
     Returns:
         list: A dataframe containing the issues. The columns of the dataframe are:
@@ -45,10 +41,7 @@ def pull_issues(
     g = Github(auth=auth)
 
     repo = g.get_repo(github_repo)
-    if end_id == -1:
-        issues = repo.get_issues(state="closed", direction="asc")[start_id:]
-    else:
-        issues = repo.get_issues(state="closed", direction="asc")[start_id:end_id]
+    issues = repo.get_issues(state="closed", direction="asc")
 
     for issue in issues:
         if issue.number > MAX_ISSUE_ID:
@@ -56,7 +49,6 @@ def pull_issues(
         if len(issue.assignees) != 1:
             continue
 
-        print(f"Storing issue {issue.number}")
         issue_info = {
             "github_id": issue.number,
             "title": issue.title,
@@ -65,11 +57,6 @@ def pull_issues(
         }
         ret.append(issue_info)
 
-    rate_limit = g.get_rate_limit()
-    core_rate_limit = rate_limit.core
-
-    print(f"Requests remaining: {core_rate_limit.remaining} / {core_rate_limit.limit}")
-    print(f"Resets at: {core_rate_limit.reset}")
     df = pd.DataFrame(ret)
     df.to_json(ISSUES_FILE, orient="records")
 
